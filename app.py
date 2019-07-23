6b62c2ebee5539efb9185b69cda118aa6de3a6aa
Ubuntu <ubuntu@ip-172-31-26-54.eu-west-1.compute.internal>
2019-07-23 17:07:55 +0000

    Fixed typos in method

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def add_task():
     return render_template("addtask.html", 
         categories=mongo.db.categories.find())
 
-@app.route('/insert_task', method=['POST'])
+@app.route('/insert_task', methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
