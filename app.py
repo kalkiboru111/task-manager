3779e16ff6e8e7598b1648d741025a5d54feebf8
Ubuntu <ubuntu@ip-172-31-26-54.eu-west-1.compute.internal>
2019-07-23 09:08:24 +0000

    Added templates

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "task manager"
+app.config["MONGO_URI"] = "mongodb+srv://root:83ZQythXiDEUTHCq@myfirstcluster-qstcj.mongodb.net/task-manager?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
